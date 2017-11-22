0d85fd0b2f9d2ffcfa8a484c36bfba745be4a92a
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-22 11:10:04 +0800

    destroy內刪除資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -33,4 +33,10 @@ class AdminPostsController extends Controller
          $post->update($request->all());
          return redirect()-> route('admin.posts.index');
       }
+      public function destroy($id)
+      {
+           Post::destroy($id);
+           return redirect()->route('admin.posts.index');
+}
+
 }

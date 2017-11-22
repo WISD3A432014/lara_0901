ac2d40f92f384f61ef92055b1134595b04260392
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-11-22 13:05:33 +0800

    新增的 Route1

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -33,24 +33,27 @@
         <form action="/admin/posts/{{$post->id}}" method="POST" role="form">
             {{csrf_field()}}
             {{ method_field('PATCH') }}
+            <form action="/admin/posts" method="POST" role="form">
+                {{ csrf_field() }}
 
-            <div class="form-group">
+                <div class="form-group">
                 <label>標題：</label>
-                <input name="title" class="form-control" placeholder="請輸入文章標題"
-                       value="{{$post->title}}">
+                    <input name="title" class="form-control" placeholder="請輸入文章標題">
+
             </div>
 
             <div class="form-group">
                 <label>內容：</label>
-                <textarea name="content" class="form-control" rows="10">{{$post->content}}</textarea>
+                <textarea name="content" class="form-control" rows="10"></textarea>
             </div>
 
             <div class="form-group">
                 <label>精選？</label>
-                <select name="is_feature" class="form-control" >
-                    <option value="0" {{ $post->is_feature?'':'SELECTED' }}>否</option>
-                    <option value="1" {{ $post->is_feature?'SELECTED':'' }}>是</option>
+                <select name="is_feature" class="form-control">
+                    <option value="0">否</option>
+                    <option value="1">是</option>
                 </select>
+
             </div>
 
             <div class="text-right">
